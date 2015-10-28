7e7fc7bc8184c248527f1812a4aa1c5e4f6b0d2f
Andrew Roberts <adroberts@gmail.com>
2015-10-27 20:50:17 -0400

    fleshed out copy to action

--- mediasort/act/copy_to.py
+++ mediasort/act/copy_to.py
@@ -1,4 +1,8 @@
+import logging
+logger = logging.getLogger(__name__)
+
 import os
+import shutil
 
 from mediasort.act import Action
 
@@ -9,10 +13,27 @@ class CopyTo(Action):
         if 'destination' not in self.options:
             raise Exception("copy_to needs a destination")
 
-        if not os.path.isdir(self.options['destination']):
-            raise Exception("copy_to's destination must be a directory that exists")
+        # if not os.path.isdir(self.options['destination']):
+        #     raise Exception("copy_to's destination must be a directory that exists")
 
-    def perform(path):
-        print "copying '%s' to '%s'" % (path, self.options.destination)
+
+    def perform(self, path, dry_run):
+        logger.info("copying '%s' to '%s'" % (path, self.options['destination']))
+        if os.path.isfile(path):
+            action = self.copy_file
+        elif os.path.isdir(path):
+            action = self.copy_dir
+        else:
+            raise Exception("Couldn't find path: %s" % path)
+
+        if not dry_run:
+            action(path, self.options['destination'])
+
+
+    def copy_file(self, src, dst_root):
+        shutil.copy(src, dst_root)
+
+    def copy_dir(self, src, dst_root):
+        shutil.copytree(src, os.path.join(dst_root, os.path.basename(src)))
 
 
